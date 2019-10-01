8caaf18247045fe18a98edff5aaf413e1271ee34
Nil riera <41672389t@insitutmontilivi.cat name = nil.riera>
2019-10-01 15:56:59 +0200

    Inicial amb buscaminas

---
+++ Jocs/Jocs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sortir = false;
+            while (!sortir)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu\n" +
+                "1- Buscaminas\n" +
+                "2- tetris\n" +
+                "3- Cubos\n" +
+                "4- Sortir");
+
+
+            }
+        }
+        static public void Buscaminas()
+        {
+            Console.WriteLine("BUSCAMINAS");
+        }
+        
+    }
+}
