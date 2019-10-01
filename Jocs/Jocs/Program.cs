8d3a90f1ab8883005d93ca27c322021e77fb6845
Nil riera <41672389t@insitutmontilivi.cat name = nil.riera>
2019-10-01 16:09:31 +0200

    afegim coses per crear conflictes

--- Jocs/Jocs/Program.cs
+++ Jocs/Jocs/Program.cs
@@ -26,11 +26,14 @@ namespace Jocs
         static public void Buscaminas()
         {
             Console.WriteLine("BUSCAMINAS");
+            Console.WriteLine("Creem un conflicte per molestar");
 	    Console.WriteLine("Afegeixo coses que es mouen per la pantalla buaaaah impresionant2")
         }
         static public void Tetris()
         {
             Console.WriteLine("Tetris");
+            Console.WriteLine("Resulta que li afegim coses al tetris perque no ens denuncin per haver fet una copia tant dolenta");
+
         }
 
     }
